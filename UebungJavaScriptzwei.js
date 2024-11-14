df560d24884ea3fcd0267fb7713b2a20257b1f57
Dirk Schef <1932163496@bildung.daa.de>
2024-11-14 14:30:25 +0100

    Codeoptimierung

--- UebungJavaScriptzwei.js
+++ UebungJavaScriptzwei.js
@@ -1,15 +1,11 @@
-
-
 /*
 ### Aufgabe 2
 Schreiben Sie ein Programm, das eine Temperatur in **Celsius** in **Fahrenheit** umrechnet und dann anschließend das Ergebnis in der Konsole ausgibt. 
 Die Umrechnungsformel lautet wie folgt: `F = 1.8 * C + 32`
 mit F: Temperatur in **Fahrenheit**;
 sowie C: Temperatur in **Celsius**
-
 > **Anmerkung**: Geben Sie das Ergebnis bitte als Antwortsatz aus!
 */
-
 
 let celsius = 0;
 let fahrenheit = 0;
@@ -45,7 +41,6 @@ function converter() {
         console.log("Ungültige Eingabe. Bitte geben Sie 'C' oder 'F' ein.");
     }
 }
-
 // Aufruf der Konvertierungsfunktion
 converter();
 
