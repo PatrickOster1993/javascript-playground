b4530b649d98f21cea8e7fa1a4e5b5526f66bcde
cyrillorisernst <maximilian.hoeltkemeier@gmail.com>
2024-11-14 14:11:59 +0100

    couldn't bear those spelling mistakes

--- aufgabe1.js
+++ aufgabe1.js
@@ -6,7 +6,6 @@ function displayPerson(){
         lieblingsessen: "Chili sin Carne oder Subway",
         }
 
-    console.log("Die Person heißt: ", person, ".")
     console.log("Eine Person namens", person.name,"ist", person.alter, "Jahre alt und ihr Lieblingsessen ist", person.lieblingsessen,".")
 }
 /* ja, ist bei Nicki abgeschrieben. Aber wenn es in meinen Augen schon die beste Lösung ist? ¯\_(ツ)_/¯ */
