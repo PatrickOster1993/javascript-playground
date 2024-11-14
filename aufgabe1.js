d9db712b5bf1fbfb4750c4a93bc00c4f9839d790
cyrillorisernst <maximilian.hoeltkemeier@gmail.com>
2024-11-14 14:28:21 +0100

    a whole commit for a single whitespace

--- aufgabe1.js
+++ aufgabe1.js
@@ -3,9 +3,9 @@ function displayPerson(){
         {
         name: "Max",
         alter: 29,
-        lieblingsessen: "Chili sin Carne oder Subway",
+        lieblingsessen: "Chili sin Carne oder Subway.",
         }
 
-    console.log("Eine Person namens", person.name,"ist", person.alter, "Jahre alt und ihr Lieblingsessen ist", person.lieblingsessen,".")
+    console.log("Eine Person namens", person.name,"ist", person.alter, "Jahre alt und ihr Lieblingsessen ist", person.lieblingsessen)
 }
 /* ja, ist bei Nicki abgeschrieben. Aber wenn es in meinen Augen schon die beste Lösung ist? ¯\_(ツ)_/¯ */
