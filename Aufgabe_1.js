5666a146f62b24aa4122780ddc36438d98a5ca54
Rick <1932164947@bildung.daa.de>
2024-11-14 13:32:13 +0100

    Aufgabe 3

--- Aufgabe_1.js
+++ Aufgabe_1.js
@@ -1,9 +1,6 @@
-
-
-
-
-function meinePerson(in_name, in_alter, in_lieblingsessen) {
+function meinePerson(in_name, in_alter, in_lieblingsessen) 
+{
     return console.log("Ich bin ", in_name," , ", in_alter," und esse gerne ", in_lieblingsessen)
-  }
+}
 
 meinePerson("Rick", 37, "pizza")    
