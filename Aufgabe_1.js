e87ac81bd9e69893522122b389da064826084851
Philipp Heidenreich <Philipp_Heidenreich@gmx.de>
2024-11-14 11:08:59 +0100

    Aufgabe 1 Döner macht schöner!   Da müssen einige Spieze dran glauben.

---
+++ Aufgabe_1.js
@@ -0,0 +1,5 @@
+let vorname = "Philipp"
+let alter = 40
+let lieblingsessen = "Döner"
+
+console.log("Hallo mein Name ist", vorname, "und ich bin", alter, "Jahre alt.", "Mein Lieblingsessen ist", lieblingsessen, ".")
