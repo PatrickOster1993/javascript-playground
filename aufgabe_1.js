76c4696d25ea613f8ef8095f8c3e4896afd2ab5b
derNico <1932163497@bildung.daa.de>
2024-11-14 10:48:45 +0100

    Aufgabe 1-Lösung

---
+++ aufgabe_1.js
@@ -0,0 +1,5 @@
+let vorname = "Nico"
+let alter = 27
+let lieblingsessen = "Veganes Hack"
+
+console.log("Hallo mein Name ist", vorname, "und ich bin", alter, "Jahre alt.", "Mein Lieblingsessen ist", lieblingsessen+ ".")
