bac6431c0f4e12fee054cfeb4f365e8247dfec0b
cyrillorisernst <maximilian.hoeltkemeier@gmail.com>
2024-11-14 13:54:59 +0100

    first three exercises finished

---
+++ aufgabe1.js
@@ -0,0 +1,11 @@
+let person = 
+    {
+    name: "Max",
+    alter: 29,
+    lieblingsessen: "Chili sin Carne oder Subway",
+    }
+
+console.log("Die Person heißt: ", person, ".")
+console.log("Eine Person namens", person.name,"ist", person.alter, "Jahre alt und ihr Lieblingsessen ist", person.lieblingsessen,".")
+
+/* ja, ist bei Nicki abgeschrieben. Aber wenn es in meinen Augen schon die beste Lösung ist? ¯\_(ツ)_/¯ */
---
+++ aufgabe2.js
@@ -0,0 +1,4 @@
+let celsiusValue = 23
+let fahrenheitValue = 1.8 * celsiusValue + 32
+
+console.log(celsiusValue, "°C equals ", fahrenheitValue, "°F.")
