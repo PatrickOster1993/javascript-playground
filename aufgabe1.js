fefcdd0a5bce2e52bdd18db9e4501ec5644530b3
unknown <Marcel>
2024-11-15 08:37:43 +0100

    aufgaben bearbeitet und in html eingebaut

---
+++ aufgabe1.js
@@ -0,0 +1,6 @@
+let person = {
+    name: "Marcel",
+    alter: 31,
+    lieblingsessen: "Lasagne",
+  }
+  console.log("Hallo", person.name, "du bist", person.alter, "und dein Lieblingsessen ist", lieblingsessen, ".")
