9bc798d25abe5071cb468b419b175e373e4286cc
maxi <maximus68@hotmail.de>
2024-11-15 14:45:58 +0100

    running90

--- runningInTheNineties/src/animations.js
+++ runningInTheNineties/src/animations.js
@@ -13,3 +13,17 @@ setInterval(function () {
     position = -100 // Setzt die Position zurück, sodass die Box wieder von links startet
   }
 }, 20) // Intervall von 20 Millisekunden
+
+let box2 = document.getElementById("animBox2") // Holt das DOM-Element für die animierte Box 2
+let position2 = 0 // Startposition der Box2
+
+// Diese Funktion wird alle 20 Millisekunden aufgerufen und bewegt die Box2
+setInterval(function () {
+  position2 += 2 // verringert die Position der Box2 um 2 Pixel
+  box2.style.right = position2 + "px" // Setzt die linke Position der Box
+
+  // Wenn die Box das Ende des Fensters erreicht, wird sie wieder von links anfangen
+  if (position2 > window.innerWidth) {
+    position2 = +110 // Setzt die Position zurück, sodass die Box wieder von links startet
+  }
+}, 20) // Intervall von 20 Millisekunden
