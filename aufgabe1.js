8fb2e2cfc72b1b897ef1e4ebf4eec6cb836c37ac
maxi <maximus68@hotmail.de>
2024-11-15 09:15:54 +0100

    Alle aufgaben von gestern

---
+++ aufgabe1.js
@@ -0,0 +1,6 @@
+let person = {
+    name: "Maximilian",
+    alter: 24,
+    lieblingsessen: "hab keins, verschlinge alles."
+}
+console.log("Mein name ist",person.name,"und ich bin",person.alter,"Jahre alt und mein lieblingsessen ist",person.lieblingsessen)
---
+++ aufgabe2.js
@@ -0,0 +1,6 @@
+let celsius = 18
+let fahrenheit = 0
+
+fahrenheit = 1.8 * celsius + 32
+
+console.log(celsius + "° Celsius sind umgerechnet", fahrenheit , "Fahrenheit")
